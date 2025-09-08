a6ba03303ce5354f527ebaeda87ed36c866deabb
Amer Alwan <ameralwan@hotmail.com>
2025-09-07 21:24:16 -0400

    rename to users

--- prisma/migrations/20250908012335_users_init/migration.sql
+++ prisma/migrations/20250908012335_users_init/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
-CREATE TABLE "public"."Users" (
+CREATE TABLE "public"."users" (
     "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
